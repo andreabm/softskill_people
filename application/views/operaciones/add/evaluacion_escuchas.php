a7a80e67b7d8cf5ae7b46d74a8c78675560b63d7
gabozizou <gmoya@serbanc.cl>
2017-01-24 12:30:30 -0300

    Evaluacion escuchas, solucion a error

--- application/views/operaciones/add/evaluacion_escuchas.php
+++ application/views/operaciones/add/evaluacion_escuchas.php
@@ -190,8 +190,8 @@
                               <td>Nota Parcial</td>
                               <td></td>
                               <td>
-                                  <input type="hidden" name="nparcial<?=$a['id_aspecto'];?>" id="nparcial<?=$a['id_aspecto'];?>" value="" readonly>
-                                  <input type="text" name="ntotal<?=$a['id_aspecto'];?>" id="ntotal<?=$a['id_aspecto'];?>" class="form-control importe_linea" value="0" readonly>
+                                  <input type="text" name="nparcial<?=$a['id_aspecto'];?>" id="nparcial<?=$a['id_aspecto'];?>" class="form-control" value="" readonly>
+                                  <input type="hidden" name="ntotal<?=$a['id_aspecto'];?>" id="ntotal<?=$a['id_aspecto'];?>" class="form-control importe_linea" value="0" readonly>
                               </td>
                             </tr>  
                   </table>            
